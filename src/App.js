7cfdd3eea6d8f84293f61c2e86d0212529a789c1
smitapatare1 <smitapatare2002>
2022-11-20 21:14:30 +0530

    react app practice

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import product1 from "./product1.js"
+import product2 from "./product2.js"
+
+
+function App() {
+  return (
+    <>
+      <product1/>
+      <product2/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/product1.js
@@ -0,0 +1,12 @@
+import "./product1.css"
+function product1(){
+    return(
+        <>
+            <div className="product1-container">
+                <h1>product 1</h1>
+
+                </div>
+        </>
+    )
+}
+export default product1 
